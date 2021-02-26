2010e665f29bb6268750b01240bfcc56a92c76c8
saketh <mkrishnasaketh2001@gmail.com>
2021-02-26 20:45:35 +0530

    final

--- dbConfig.js
+++ dbConfig.js
@@ -2,18 +2,18 @@ const mysql = require('mysql2');
 
 const conn = mysql.createConnection({
   // for offline mysql - "practice" online mongo
-  host: 'localhost',
-  user: 'root',
-  password: 'saketh4147',
-  database: 'rms',
+  // host: 'localhost',
+  // user: 'root',
+  // password: 'saketh4147',
+  // database: 'rms',
     
 
 
   // online mysql
-    // host: 'bq35ggmveksgiyapykyw-mysql.services.clever-cloud.com',
-    // user: 'ujvkzd5a6z7uzrc1',
-    // password: '59pm9QME1GTHHbxhUWkE',
-    // database: 'bq35ggmveksgiyapykyw',
+    host: 'bq35ggmveksgiyapykyw-mysql.services.clever-cloud.com',
+    user: 'ujvkzd5a6z7uzrc1',
+    password: '59pm9QME1GTHHbxhUWkE',
+    database: 'bq35ggmveksgiyapykyw',
 }); 
   conn.connect((err)=>{
     if(err)
--- yelp.js
+++ yelp.js
@@ -17,12 +17,12 @@ const commentRoutes = require("./routes/comments"),
 	indexRoutes = require("./routes/index");
 
 // for offline mysql - "practice" online mongo
-const DB_URL = process.env.DB_URL || "mongodb+srv://saketh:saketh@cluster0.np0za.mongodb.net/PMS?retryWrites=true&w=majority";
+// const DB_URL = process.env.DB_URL || "mongodb+srv://saketh:saketh@cluster0.np0za.mongodb.net/PMS?retryWrites=true&w=majority";
 
 
 
 // for online mysql
-// const DB_URL = process.env.DB_URL || "mongodb+srv://saketh:saketh@cluster0.np0za.mongodb.net/rms?retryWrites=true&w=majority";
+const DB_URL = process.env.DB_URL || "mongodb+srv://saketh:saketh@cluster0.np0za.mongodb.net/rms?retryWrites=true&w=majority";
 
 const PORT = process.env.PORT || 3000;
 // const IP = process.env.IP || "127.0.0.1"
@@ -79,18 +79,18 @@ module.exports = () => {
 	var conn = mysql.createConnection({
 
 		// for offline mysql - "practice" online mongo
-		host: 'localhost',
-		user: 'root', 
-		password: 'saketh4147',
-		database: 'rms',
+		// host: 'localhost',
+		// user: 'root', 
+		// password: 'saketh4147',
+		// database: 'rms',
 
 
 
 		//for online mysql
-		// host: 'bq35ggmveksgiyapykyw-mysql.services.clever-cloud.com',
-  		// user: 'ujvkzd5a6z7uzrc1',
-  		// password: '59pm9QME1GTHHbxhUWkE',
-  		// database: 'bq35ggmveksgiyapykyw',
+		host: 'bq35ggmveksgiyapykyw-mysql.services.clever-cloud.com',
+  		user: 'ujvkzd5a6z7uzrc1',
+  		password: '59pm9QME1GTHHbxhUWkE',
+  		database: 'bq35ggmveksgiyapykyw',
 	});
 
 	conn.connect((err) => {
