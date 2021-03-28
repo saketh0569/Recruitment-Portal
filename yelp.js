123d8bde0269ce5c28597858386d9a0858ca7044
saketh <mkrishnasaketh2001@gmail.com>
2021-03-28 23:45:12 +0530

    online sql

--- yelp.js
+++ yelp.js
@@ -11,18 +11,16 @@ const express = require('express'),
 	flash = require('connect-flash'),
 	logger = require('morgan')
 Comment = require("./models/comment.js");
- 
+
 const commentRoutes = require("./routes/comments"),
 	moviesRoutes = require("./routes/movies"),
 	indexRoutes = require("./routes/index");
 
 // for offline mysql - "practice" online mongo
-// const DB_URL = process.env.DB_URL || "mongodb+srv://saketh:saketh@cluster0.np0za.mongodb.net/PMS?retryWrites=true&w=majority";
-
-
+const DB_URL = process.env.DB_URL || "mongodb+srv://saketh:saketh@cluster0.np0za.mongodb.net/PMS?retryWrites=true&w=majority";
 
 // for online mysql
-const DB_URL = process.env.DB_URL || "mongodb+srv://saketh:saketh@cluster0.np0za.mongodb.net/rms?retryWrites=true&w=majority";
+// const DB_URL = process.env.DB_URL || "mongodb+srv://saketh:saketh@cluster0.np0za.mongodb.net/rms?retryWrites=true&w=majority";
 
 const PORT = process.env.PORT || 3000;
 // const IP = process.env.IP || "127.0.0.1"
@@ -79,18 +77,18 @@ module.exports = () => {
 	var conn = mysql.createConnection({
 
 		// for offline mysql - "practice" online mongo
-		// host: 'localhost',
-		// user: 'root', 
-		// password: 'saketh4147',
-		// database: 'rms',
+		host: 'localhost',
+		user: 'root', 
+		password: 'saketh4147',
+		database: 'rms',
 
 
 
 		//for online mysql
-		host: 'bq35ggmveksgiyapykyw-mysql.services.clever-cloud.com',
-  		user: 'ujvkzd5a6z7uzrc1',
-  		password: '59pm9QME1GTHHbxhUWkE',
-  		database: 'bq35ggmveksgiyapykyw',
+		// host: 'bq35ggmveksgiyapykyw-mysql.services.clever-cloud.com',
+		// user: 'ujvkzd5a6z7uzrc1',
+		// password: '59pm9QME1GTHHbxhUWkE',
+		// database: 'bq35ggmveksgiyapykyw',
 	});
 
 	conn.connect((err) => {
